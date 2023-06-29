0871e05d4bb9d66cc247fc7dc94b28e95afb81b7
SEUNGJUN JEONG <jsj9620@hufs.ac.kr>
2023-06-29 20:43:52 +0900

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,126 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useEffect, useRef } from "react";
+import Webcam from "react-webcam";
+import { css } from "@emotion/css";
+import { Camera } from "@mediapipe/camera_utils";
+import { Hands, Results } from "@mediapipe/hands";
+import { drawCanvas } from "./utils/drawCanvas";
 
-function App() {
+const App = () => {
+  const webcamRef = useRef<Webcam>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const resultsRef = useRef<Results>();
+
+  /**
+   * 検出結果（フレーム毎に呼び出される）
+   * @param results
+   */
+  const onResults = useCallback((results: Results) => {
+    resultsRef.current = results;
+
+    const canvasCtx = canvasRef.current!.getContext("2d")!;
+    drawCanvas(canvasCtx, results);
+  }, []);
+
+  // 初期設定
+  useEffect(() => {
+    const hands = new Hands({
+      locateFile: (file) => {
+        return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
+      },
+    });
+
+    hands.setOptions({
+      maxNumHands: 2,
+      modelComplexity: 1,
+      minDetectionConfidence: 0.5,
+      minTrackingConfidence: 0.5,
+    });
+
+    hands.onResults(onResults);
+
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null
+    ) {
+      const camera = new Camera(webcamRef.current.video!, {
+        onFrame: async () => {
+          await hands.send({ image: webcamRef.current!.video! });
+        },
+        width: 1280,
+        height: 720,
+      });
+      camera.start();
+    }
+  }, [onResults]);
+
+  /** 検出結果をconsoleに出力する */
+  const OutputData = () => {
+    const results = resultsRef.current!;
+    console.log(results.multiHandLandmarks);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.container}>
+      {/* capture */}
+      <Webcam
+        audio={false}
+        style={{ visibility: "hidden" }}
+        width={1280}
+        height={720}
+        ref={webcamRef}
+        screenshotFormat="image/jpeg"
+        videoConstraints={{ width: 1280, height: 720, facingMode: "user" }}
+      />
+      {/* draw */}
+      <canvas
+        ref={canvasRef}
+        className={styles.canvas}
+        width={1280}
+        height={720}
+      />
+      {/* output */}
+      <div className={styles.buttonContainer}>
+        <button className={styles.button} onClick={OutputData}>
+          Output Data
+        </button>
+      </div>
     </div>
   );
-}
+};
+
+// ==============================================
+// styles
+
+const styles = {
+  container: css`
+    position: relative;
+    width: 100vw;
+    height: 100vh;
+    overflow: hidden;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  `,
+  canvas: css`
+    position: absolute;
+    width: 1280px;
+    height: 720px;
+    background-color: #fff;
+  `,
+  buttonContainer: css`
+    position: absolute;
+    top: 20px;
+    left: 20px;
+  `,
+  button: css`
+    color: #fff;
+    background-color: #0082cf;
+    font-size: 1rem;
+    border: none;
+    border-radius: 5px;
+    padding: 10px 10px;
+    cursor: pointer;
+  `,
+};
 
 export default App;
